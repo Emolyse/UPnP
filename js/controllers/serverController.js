66d29b58b6f85b784706635f19efb96c0bab1e0b
Rémy <contact@remy-drouet.fr>
2015-12-11 18:21:30 +0100

    Avancement navigation-explorer

--- js/controllers/serverController.js
+++ js/controllers/serverController.js
@@ -1,26 +1,22 @@
-module.exports = function(app,utils,parser) {
+module.exports = function(app,utils) {
     app.controller("sc", ['$scope','$location', '$http',function ($scope,$location,$http) {
         var ctrl = this;
         $http.get("/getContext").success(function (data) {
             //ctrl.context = data;
             ctrl.context = {bricks:{}};
             for(var i in data.bricks){
-                console.log();
                 ctrl.context.bricks[data.bricks[i].id]=data.bricks[i];
             }
-            console.log(data);
-            console.log("ctrl.context.bricks", ctrl.context.bricks);
 
             utils.io.on("brickAppears", function (data) {
-                console.log("brickAppears", data);
                 ctrl.context.bricks[data.id] = data;
                 $scope.$apply();
             });
 
             utils.io.on("brickDisappears", function (data) {
-                console.log("brickDisappears", data);
                 delete ctrl.context.bricks[data.brickID];
-            })
+            });
+            ctrl.context.explorer = [];
         });
 
         /**
@@ -36,10 +32,30 @@ module.exports = function(app,utils,parser) {
                 , "Browse"
                 , [dirId]
                 , function (str) {
+                    var parser = new DOMParser();
+                    ctrl.context.explorer.splice(0,ctrl.context.explorer.length);
                     var doc = parser.parseFromString(str, "text/xml");
-                    console.log(doc);
                     var res = doc.querySelector("Result");
-                    console.log(res);
+                    var content = parser.parseFromString(res.textContent, "text/xml");
+                    var containers = content.querySelectorAll("container");
+                    for(var i=0;i<containers.length;i++){
+                        var dir = {};
+                        var attr = containers[i].attributes;
+                        if(attr)
+                            for(var j=0;j<attr.length;j++){
+                                dir[attr[j].name]=attr[j].value;
+                            }
+                        var children = containers[i].children;
+                        if(children)
+                            for(var j=0;j<children.length;j++){
+                                var name = children[j].tagName+"";
+                                //name = name.split(':');
+                                //name = name[name.length-1];
+                                dir[name]=children[i].textContent;
+                            }
+                        ctrl.context.explorer.push(dir);
+                    }
+                    $scope.$apply();
                 });
         }
     }]);
--- js/directives/mediaExplorer.js
+++ js/directives/mediaExplorer.js
@@ -5,8 +5,49 @@ module.exports = function(app,utils) {
             ,templateUrl:'templates/media-explorer.html'
             ,scope:{
                 bricks:"=bricks"//
+                ,explorer:"=explorer"
                 ,title:"@title"}//Chaine passée directement
             ,controllerAs:"mc"
+            ,controller:function ($scope,$location,$http) {
+                var ctrl = this;
+                ctrl.breadCrumb = [$scope.title];
+                ctrl.bricks = $scope.bricks;
+                ctrl.explorer = $scope.explorer;
+                ctrl.Browse = function (brick,dirId) {
+                    console.log("bananas");
+                    if(!dirId)
+                        dirId = 0;
+                    utils.call(brick.id
+                        , "Browse"
+                        , [dirId]
+                        , function (str) {
+                            var parser = new DOMParser();
+                            ctrl.explorer.splice(0,ctrl.explorer.length);
+                            var doc = parser.parseFromString(str, "text/xml");
+                            var res = doc.querySelector("Result");
+                            var content = parser.parseFromString(res.textContent, "text/xml");
+                            var containers = content.querySelectorAll("container");
+                            for(var i=0;i<containers.length;i++){
+                                var dir = {};
+                                var attr = containers[i].attributes;
+                                if(attr)
+                                    for(var j=0;j<attr.length;j++){
+                                        dir[attr[j].name]=attr[j].value;
+                                    }
+                                var children = containers[i].children;
+                                if(children)
+                                    for(var j=0;j<children.length;j++){
+                                        var name = children[j].tagName+"";
+                                        //name = name.split(':');
+                                        //name = name[name.length-1];
+                                        dir[name]=children[i].textContent;
+                                    }
+                                ctrl.explorer.push(dir);
+                            }
+                            $scope.$apply();
+                        });
+                }
+            }
         }
     });
 }
