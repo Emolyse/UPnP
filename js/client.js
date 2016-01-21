912ce1158156a6eedd053aa607e7e78b4a14b91b
alizeear <arn.alizee@gmail.com>
2016-01-21 22:16:01 +0100

    player design

--- js/client.js
+++ js/client.js
@@ -1,7 +1,13 @@
 var utils = require("./utils.js");
 utils.initIO();
 
-var app = angular.module("UPnP",['ngMaterial','ui.router','angular-toArrayFilter']);
+var app = angular.module("UPnP",['ngMaterial','ui.router','angular-toArrayFilter'])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('orange')
+            .warnPalette('red');
+    });
 
 require( "./controllers/serverController.js" )(app,utils);
 require( "./directives/mediaExplorer.js" )(app,utils);
